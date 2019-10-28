bfc0c856f8666f703c9dcb3f17d2a71fdea16201
Salomé Pelaez <spsalome@gmail.com>
2019-10-28 17:59:34 -0500

    Fonts added

--- Ace Invaders/Assets/Scripts/Dialogue.cs
+++ Ace Invaders/Assets/Scripts/Dialogue.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshProUGUI intro;
-   // public List<TextMeshProUGUI> story = new List<TextMeshProUGUI>(4);
+    public Text intro;
+    // public List<TextMeshProUGUI> story = new List<TextMeshProUGUI>(4);
 
+    public void Awake()
+    {
+        intro = GameObject.Find("Dialogue").GetComponent<Text>();
+    }
     public void Story()
     {
-        InvokeRepeating("Talk", 0, 1);
+        StartCoroutine("PrintMessages");
     }
 
-    private string Talk()
+    IEnumerator PrintMessages()
     {
-        intro.text = "ahgdhga";
-        return intro.text;
+        intro.text = "You must find the way to save the city.";
+   
+        yield return new WaitForSeconds(5);
+
+        intro.text = "But you know...";
+
+        yield return new WaitForSeconds(5);
+
+        intro.text = "... sometimes the only way to be successful ";
+
+        yield return new WaitForSeconds(5);
+
+        intro.text = "is fight!";
+
+        yield return new WaitForSeconds(5);
+
+        intro.text = "";
     }
 }
