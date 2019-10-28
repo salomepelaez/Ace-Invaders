fcda0ccf6404464f582d9601be784211a3cb82a4
Salomé Pelaez <spsalome@gmail.com>
2019-10-28 13:24:48 -0500

    Materials added

--- Ace Invaders/Assets/Scripts/Manager.cs
+++ Ace Invaders/Assets/Scripts/Manager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float speed = 0.1f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
     }
 }
