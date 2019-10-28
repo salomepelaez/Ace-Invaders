8e3eefd6e44247880e8e10bf07cf46ec26bf5289
Salomé Pelaez <spsalome@gmail.com>
2019-10-28 17:03:28 -0500

    Dialogue script started

---
+++ Ace Invaders/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI intro;
+   // public List<TextMeshProUGUI> story = new List<TextMeshProUGUI>(4);
+
+    public void Story()
+    {
+        InvokeRepeating("Talk", 0, 1);
+    }
+
+    private string Talk()
+    {
+        intro.text = "ahgdhga";
+        return intro.text;
+    }
+}
