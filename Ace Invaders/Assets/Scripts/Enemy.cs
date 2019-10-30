f2a974a5f9313aa547c15d1185ac6214b2571dd9
Salomé Pelaez <spsalome@gmail.com>
2019-10-30 13:23:18 -0500

    Weapon and enemies scripts added

---
+++ Ace Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject enemie;
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    int enemies = 3;
+    
+    void Initialize()
+    {
+        for(int x = 0; x < enemies; x++)
+        {
+            Vector3 pos = new Vector3();
+            enemie = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            pos.x = Random.Range(-8.17f, 8.17f);
+            pos.y = Random.Range(-3, 5);
+            pos.z = -78;
+            enemie.transform.position = pos;
+        }
+    }
+
+    float speed = 0.5f;
+    void Movement()
+    {
+        switch(Random.Range(0, 1))
+        {
+            case 0:
+                transform.position += transform.right * speed;
+                break;
+        }
+    }
+}
---
+++ Ace Invaders/Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+
+    void Update()
+    {
+        if(Input.GetKey((KeyCode.X)))
+        {
+            bullet = Instantiate(bullet) as GameObject;
+        }
+    }
+}
