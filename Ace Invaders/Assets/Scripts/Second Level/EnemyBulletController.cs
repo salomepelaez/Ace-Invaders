6ac785ea171fff1a08128dcc34058315b461c391
Salomé Pelaez <spsalome@gmail.com>
2019-10-31 23:25:07 -0500

    Revert "Revert "Enemy can make damage(started)""
    This reverts commit 7fb0c72bd58d60adb6d67b34aead832d79ade135.

---
+++ Ace Invaders/Assets/Scripts/Second Level/EnemyBulletController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+   public static int weaponDamage = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ace Invaders/Assets/Scripts/Second Level/EnemyWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    public GameObject bullet;
+    float speed = 800f;
+
+    void Awake()
+    {
+        transform.tag = "Bullet";
+    }
+
+    void Start()
+    {
+        InvokeRepeating("DoDamage", 2f, 2f);                
+    }   
+
+    void DoDamage()
+    {
+        GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+        instBullet.transform.Rotate(Vector3.up * 90);
+        Rigidbody rigidbody = instBullet.GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.forward * speed);
+
+        Destroy(instBullet, 3f);
+    }
+}
